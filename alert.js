2d2c80bcd8619845ba56340819fb5a92bf94b32a
Sidik Suhendar <37766433+Sidik18@users.noreply.github.com>
2021-01-21 16:50:34 +0700

    Add files via upload

---
+++ alert.js
@@ -0,0 +1,14 @@
+
+
+  Swal.fire({
+    title: '<strong>Peringatan Protokol Kesehatan!</strong>',
+    icon: 'info',
+    html:
+      'Mengingat situasi dan kondisi di masa pandemi Covid-19 Tanpa mengurangi rasa hormat, para tamu undangan yang bersedia menghadiri acara dimohon kesediannya untuk ' +
+      'mematuhi protokol kesehatan',
+   
+    focusConfirm: false,
+    confirmButtonText:
+      '<i class="fa fa-thumbs-up"></i> Setuju',
+    confirmButtonAriaLabel: 'Thumbs up, great!'
+  })
